379c9f9e54144f608dd7d96a14517b52cb079f84
sristikashakya <sristikashakya123@gmail.com>
2018-04-18 15:44:57 +0545

    adding appbar and drawer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import Header from './components/Header';
 class App extends Component {
   
   render() {
     return (
       <div>
-        This is my app.
+        <MuiThemeProvider>
+         <Header />
+         </MuiThemeProvider>
       </div>
     )
   }
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+class Header extends Component{
+	constructor(props){
+		super(props)
+		this.state = {open:false}
+	}
+	render(){
+		return(
+				<div>
+					<AppBar title="Sgaramatha" 
+					onLeftIconButtonClick={()=>this.toggleSidebar()} />
+					<Drawer
+						open= {this.state.open}
+						docked={false}
+						onRequestChange = {
+							()=> this.toggleSidebar()
+						}>
+						<MenuItem>
+							FirstItem
+						</MenuItem>
+					</Drawer>
+				</div>
+			)
+	}
+	toggleSidebar(){
+		this.setState({open:!this.state.open})
+	}
+}
+export default Header;
