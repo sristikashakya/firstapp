3489b434ffbd603ad39ca17071386429622193c8
sristikashakya <sristikashakya123@gmail.com>
2018-04-26 09:18:49 +0545

    adding API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
 import Header from './components/Header';
-import Table from './components/Tables';
 import Login from './components/Login';
 class App extends Component {
-  person = {
-    email: 'enter you email',
-    API: 'API'
-  }
   render() {
     return (
       <div>
         <MuiThemeProvider>
          <Header />
-         <Table />
-          <Login detail = {this.person} />
+          <Login />
          </MuiThemeProvider>
 
       </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,23 +3,48 @@ class Login extends Component{
 	constructor(props){
 		super(props)
 		this.state = {
-				email: props.detail.email,
-				API: props.detail.API
+				email: '',
+				API: ''
 		}
 	}
 	render(){
 		return(
 				<div>
 					<form>
-						Email= <input type = 'text' value = {this.state.email} onChange = {(event) => this.onChangeEmail(event)}/><br /><br/>
-						API Key = <input type = 'text' value = {this.state.API} onChange = {(event) => this.onChangeAPI(event)}/><br/><br/>
-						<input type="Submit" onClick = {() => this.handleClick()}/>
+						Email= <input type = 'text' placeholder='email' value = {this.state.email} onChange = {(event) => this.onChangeEmail(event)}/><br /><br/>
+						API Key = <input type = 'text' placeholder='apikey' value = {this.state.API} onChange = {(event) => this.onChangeAPI(event)}/><br/><br/>
+						<input type="Submit" onClick = {(event) => this.handleClick(event)}/>
 					</form>
 				</div>
 			)
 	}
-	handleClick = function(){
-		console.log(this.state)
+	handleClick = function(event){
+		event.preventDefault()
+		fetch('https://api.rebrandly.com/v1/account',
+		{
+			headers: {
+				apikey: this.state.API
+			}
+		})
+		.then(response =>
+		{
+			if(response.ok){
+				response.json()
+				.then(data => {
+					console.log(data)
+					if(data.email === this.state.email){
+						console.log("right user")
+					}
+					else{
+						alert('unauthorized username')
+					}
+				})
+			}
+			else{
+				alert(response.statusText)
+			}	
+		})
+		
 	}
 	onChangeEmail = function(event){
 		this.setState({
@@ -31,5 +56,6 @@ class Login extends Component{
    				
    				API: event.target.value
    		})
-	}}
+	}
+}
 export default Login;
